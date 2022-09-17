b07bc23de2e953c8ebf07248998e48c3a395df38
kawamataryo <ba068082@gmail.com>
2022-09-17 18:11:55 +0900

    fix: vue3 not being able to retrieve nested slot text

---
+++ tests-vue3/tests/fixtures/WrappedWordHighlighter.vue
@@ -0,0 +1,35 @@
+<template>
+  <VueWordHighlighter
+    :query="query"
+    :case-sensitive="caseSensitive"
+    :split-by-space="splitBySpace"
+  >
+    <slot />
+  </VueWordHighlighter>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import VueWordHighlighter from "../../../vue-word-highlighter/src/components";
+
+export default defineComponent({
+  name: "WrappedWordHighlighter",
+  components: {
+    VueWordHighlighter,
+  },
+  props: {
+    query: {
+      type: String,
+      required: true,
+    },
+    caseSensitive: {
+      type: Boolean,
+      required: false,
+    },
+    splitBySpace: {
+      type: Boolean,
+      required: false,
+    },
+  },
+});
+</script>
