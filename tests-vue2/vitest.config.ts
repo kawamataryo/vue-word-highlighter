bf0212dfc656388e44f75243d1e8fb7e3befc045
kawamataryo <ba068082@gmail.com>
2022-10-08 23:12:46 +0900

    test: migrate vitest from jest

---
+++ tests-vue2/vitest.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from "vitest/config";
+import vue from "@vitejs/plugin-vue2";
+
+export default defineConfig({
+  plugins: [vue()],
+  test: {
+    globals: true,
+    environment: "jsdom",
+  },
+});
---
+++ tests-vue3/vitest.config.ts
@@ -0,0 +1,10 @@
+import vue from "@vitejs/plugin-vue";
+import { defineConfig } from "vitest/config";
+
+export default defineConfig({
+  plugins: [vue()],
+  test: {
+    globals: true,
+    environment: "jsdom",
+  },
+});
