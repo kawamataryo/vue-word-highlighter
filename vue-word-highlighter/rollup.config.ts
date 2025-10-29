afd886b3acde3281e5dd9b1490ecf7b2d9183da5
kawamataryo <ba068082@gmail.com>
2025-10-29 15:53:57 +0900

    fix: build error

--- vue-word-highlighter/rollup.config.ts
+++ vue-word-highlighter/rollup.config.ts
@@ -1,46 +1,48 @@
+import { createRequire } from "node:module";
 import terser from "@rollup/plugin-terser";
 import { defineConfig } from "rollup";
 import dts from "rollup-plugin-dts";
 import typescript from "rollup-plugin-typescript2";
-import pkg from "./package.json" assert { type: "json" };
+const require = createRequire(import.meta.url);
+const pkg = require("./package.json");
 
 export default defineConfig([
-	{
-		plugins: [typescript(), terser()],
-		external: ["vue-demi"],
-		input: "src/index.ts",
-		output: [
-			{
-				format: "esm",
-				compact: true,
-				file: pkg.module,
-				sourcemap: true,
-			},
-			{
-				compact: true,
-				format: "cjs",
-				file: pkg.main,
-				sourcemap: true,
-				exports: "default",
-			},
-			{
-				file: pkg.unpkg,
-				format: "umd",
-				name: "VueWordHighlighter",
-				compact: true,
-				sourcemap: true,
-				globals: {
-					"vue-demi": "VueDemi",
-				},
-			},
-		],
-	},
-	{
-		input: "src/index.ts",
-		plugins: [dts()],
-		output: {
-			file: "dist/index.d.ts",
-			format: "es",
-		},
-	},
+  {
+    plugins: [typescript({ tsconfig: "./tsconfig.json" }), terser()],
+    external: ["vue-demi"],
+    input: "src/index.ts",
+    output: [
+      {
+        format: "esm",
+        compact: true,
+        file: pkg.module,
+        sourcemap: true,
+      },
+      {
+        compact: true,
+        format: "cjs",
+        file: pkg.main,
+        sourcemap: true,
+        exports: "default",
+      },
+      {
+        file: pkg.unpkg,
+        format: "umd",
+        name: "VueWordHighlighter",
+        compact: true,
+        sourcemap: true,
+        globals: {
+          "vue-demi": "VueDemi",
+        },
+      },
+    ],
+  },
+  {
+    input: "src/index.ts",
+    plugins: [dts()],
+    output: {
+      file: "dist/index.d.ts",
+      format: "es",
+    },
+  },
 ]);
